d1b7447cff433b341859daa69bb6f8499f34d2ba
EvgeniyKazancev <ekazancev777@gmail.com>
2023-01-31 16:20:12 +0300

    Up

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,11 @@
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class main {
     public static void main(String[] args) {
 
-        University university =new University( "1","Ural Politech University","UPI",1930, University.StudyProfile.MEDICINE);
-        Student student = new Student( "Evgeniy","1",2,4);
-        System.out.println(university);
-        System.out.println(student);
+        University university = new University( "1","Ural Politech University","UPI",1930, University.StudyProfile.MEDICINE);
+        Student student = new Student( "Evgeniy Kazantsev","1",2,4);
+        System.out.println(ToStringBuilder.reflectionToString(university));
+        System.out.println(ToStringBuilder.reflectionToString(student));
     }
 }
